2418a405bdc4b7c8820e9f2c96ef551c393c675f
Tyler-pierce <tyler.pierce@gmail.com>
2019-12-13 08:11:36 -0800

    another build server fix attempt

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -4,7 +4,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = (env, options) => ({
   optimization: {
